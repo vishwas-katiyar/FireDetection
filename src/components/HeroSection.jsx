23629538c6231cd356bd7fd63d67a14e1d5938a9
Vishwas Katiyar <Vishwas Katiyar@inindcrws.com>
2024-07-07 23:36:46 +0530

    lock updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Typography } from "@material-tailwind/react";
-import FeatureSection from "./Featuresection";
+import FeatureSection from "./FeatureSection";
 
 function HeroSection() {
   return (
